034bbcca8b170236b9f9e2c2b521cf1def5f8ebe
Vladyslav Shupliakov <shupliakovvl@gmail.com>
2025-03-14 15:17:14 +0200

    [pm] - initial commit

---
+++ src/main/java/com/pb/task/pb_task/PbTaskApplication.java
@@ -0,0 +1,13 @@
+package com.pb.task.pb_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PbTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pb/task/pb_task/PbTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pb.task.pb_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
